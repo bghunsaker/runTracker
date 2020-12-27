90bc2c7f2c560989507fb3d880e09920003a570c
Bryson Hunsaker <bhunsak@siue.edu>
2020-12-27 16:56:52 -0600

    Now calculates weekly avg of previous 4 complete weeks

--- runTracker.py
+++ runTracker.py
@@ -12,7 +12,7 @@ from google.auth.transport.requests import Request
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 # set this to Sunday prior to first recorded run
 START_DATE = datetime.date.fromisoformat("2020-05-17")
-# this sets how many miles each dot represents in our Weekly Totals graph 
+# this sets how many miles each dot represents in our Weekly Totals graph
 GRAPH_INTERVAL = .5
 
 
@@ -29,7 +29,8 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open("token.pickle", "wb") as token:
@@ -42,17 +43,13 @@ def main():
     year_ago = datetime.datetime.utcnow() - datetime.timedelta(days=365)
     year_ago = year_ago.isoformat() + "Z"
     print("Getting all runs logged in the past year:")
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=year_ago,
-            maxResults=300,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
+    events_result = (service.events().list(
+        calendarId="primary",
+        timeMin=year_ago,
+        maxResults=300,
+        singleEvents=True,
+        orderBy="startTime",
+    ).execute())
     events = events_result.get("items", [])
     miles_total = 0.0
     ctr = 0
@@ -65,41 +62,52 @@ def main():
     print("__________________________")
     print("|  # |   Date     | Miles|")
     print("__________________________")
-
+    
+    # process and output date and mileage of each individual run
     for event in events:
         if "ran " in event["summary"]:
             ctr += 1
             # if first_run is unpopulated, get the date of the first logged run
             if first_run is None:
-                first_run = datetime.date.fromisoformat(
-                    event["start"].get("dateTime", event["start"].get("date"))
-                )
+                first_run = datetime.date.fromisoformat(event["start"].get(
+                    "dateTime", event["start"].get("date")))
             # chop off the word 'ran '
             miles = event["summary"].partition(" ")[2]
             # chop off the ':' if it exists and anything that follows
             miles = miles.partition(":")[0]
             start = event["start"].get("dateTime", event["start"].get("date"))
             miles_total = miles_total + float(miles)
-            runs_list.append([datetime.date.fromisoformat(start), float(miles)])
+            runs_list.append(
+                [datetime.date.fromisoformat(start),
+                 float(miles)])
             # individual run dates and mileages printed here
-            print("|", format(ctr, "2d"), "|", start, "|", format(float(miles), ".1f"), " |")
+            print("|", format(ctr, "2d"), "|", start, "|",
+                  format(float(miles), ".1f"), " |")
     print("__________________________\n")
-    print("Total mileage:", format(miles_total, ".1f"), " miles")
-    print(
-        "Average miles per week:",
-        format(miles_total / ((now - first_run).days / 7), ".1f"),
-    )
-    print("Average miles per run:", format(miles_total / ctr, ".1f"))
+
+    # output and graph weekly totals
     print("\nWeekly totals starting", START_DATE)
     print("Each dot represents", GRAPH_INTERVAL, "miles run that week")
-    find_weekly_totals(now, START_DATE, runs_list)
+    prev_month_weekly_avg = []
+    find_weekly_totals(now, START_DATE, runs_list, prev_month_weekly_avg)
+
+    # some stats
+    print("\nTotal mileage:", format(miles_total, ".1f"), " miles")
+    print(
+        "Average weekly mileage:",
+        format(miles_total / ((now - first_run).days / 7), ".1f"),
+    )
+    print("Average weekly mileage for previous four complete weeks",
+          format(prev_month_weekly_avg[0], ".1f"))
+    print("Average miles per run:", format(miles_total / ctr, ".1f"))
 
 
-def find_weekly_totals(now, START_DATE, runs_list):
+def find_weekly_totals(now, START_DATE, runs_list, prev_month_weekly_avg):
     d = timedelta(days=7)
     i = 0
     week_ctr = 1
     cur_date = START_DATE
+    avg = 0
 
     while i <= (now - START_DATE).days:
         i += 7
@@ -108,15 +116,27 @@ def find_weekly_totals(now, START_DATE, runs_list):
         for run in runs_list:
             if run[0] < cur_date and run[0] >= cur_date - d:
                 cur_week_total += run[1]
-        # setup for and create one row per week of dots, each representing .5 mi ran
+
+        # collect data for and calculate average over the previous 4 complete
+        # weeks (don't count week in progress)
+        days_sofar_this_week = (now - START_DATE).days % 7
+        # we want to make sure we're in a week after 4 weeks before the start
+        # of the one in progress but before the one in progress
+        if (cur_date >= (now - (timedelta(days=(28 + days_sofar_this_week))))
+                and cur_date < (now - timedelta(days=days_sofar_this_week))):
+            avg += cur_week_total
+
+        # setup for and create one row of dots per week of dots, each representing .5 mi ran
         graph_string = ""
         j = 0
         while j < cur_week_total:
             graph_string += "."
             j += GRAPH_INTERVAL
 
-        print(format(week_ctr, "2d"), ":", "{:>4.1f}".format(cur_week_total), graph_string)
+        print(format(week_ctr, "2d"), ":", "{:>4.1f}".format(cur_week_total),
+              graph_string)
         week_ctr += 1
+    prev_month_weekly_avg.append(avg / 4)
 
 
 if __name__ == "__main__":
